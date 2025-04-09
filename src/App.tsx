cf1f7e9a93d34e29733f5f372a95d704b4ec3911
Sherin <68326500+Sherinsedalo@users.noreply.github.com>
2025-04-09 20:38:45 +0200

    Adding the about page v1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Footer } from '../src/components/Home/Footer';
 import Home from './pages/Home';
 import { Navbar } from './components/Home/navbar';
+import About from './pages/About'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
       <Footer />
     </>
